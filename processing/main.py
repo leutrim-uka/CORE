31ac370b48393314d54b7ea3d61a10b2bee5343d
Leutrim Uka <67911249+leutrim-uka@users.noreply.github.com>
2024-07-21 21:08:34 +0200

    Update main.py

--- processing/main.py
+++ processing/main.py
@@ -5,24 +5,15 @@ from tqdm import tqdm
 from config import data_dir, results_dir
 from title_processor import TitleProcessor
 
-def init_worker(data_dir, results_dir):
-    global processor
-    processor = TitleProcessor(data_dir, results_dir)
-
-def process_file(filename):
-    global processor
-    processor.process_file(filename)
 
 def main():
+    processor = TitleProcessor(data_dir, results_dir)
+
     start_time = time.time()
     filenames = [filename for filename in os.listdir(data_dir) if filename.endswith(".json.xz")]
 
-    with tqdm(total=len(filenames)) as pbar:
-        with multiprocessing.Pool(initializer=init_worker, initargs=(data_dir, results_dir)) as pool:
-            for filename in filenames:
-                pool.apply_async(process_file, args=(filename,), callback=lambda _: pbar.update(1))
-            pool.close()
-            pool.join()
+    for filename in filenames:
+        processor.process_file(filename)
 
     elapsed_time = time.time() - start_time
     print(f"Language processing for {len(filenames)} files completed in {elapsed_time:.2f} seconds")
