10dbbbcbb6c28d891df8671fa92b421e44d324c0
Leutrim Uka <67911249+leutrim-uka@users.noreply.github.com>
2024-07-21 19:24:58 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,10 +1,22 @@
 #!/bin/bash
 
+# Try to connect to the server with retries
+for i in {1..5}; do
+    if curl -fsSL http://localhost:11434 > /dev/null; then
+        echo "Ollama server is up!"
+        break
+    else
+        echo "Waiting for Ollama server..."
+        sleep 5
+    fi
+done
+
+# Install the model and run the Python script
 echo "Installing the model"
 
 # Install the llama2 model
-curl -X POST -H "Content-Type: application/json" -d '{"name":"llama2"}' http://ollama-server-title-detection:11434/api/pull
+curl -s -o /dev/null -X POST -H "Content-Type: application/json" -d '{"name":"mistral"}' http://localhost:11434/api/pull
 
 echo "Installed the model"
 
-python ./processing/main.py
+python3 ./processing/main.py
